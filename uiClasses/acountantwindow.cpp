1b5b05560bb488875223b411ef6b7b2fc2f911b4
SayedAliAmirshahkarami <SayedAliAmirshahkarami@gmail.com>
2018-12-07 16:33:50 +0330

    addbook created...

--- uiClasses/acountantwindow.cpp
+++ uiClasses/acountantwindow.cpp
@@ -6,6 +6,7 @@ AcountantWindow::AcountantWindow(QWidget *parent) :
     ui(new Ui::AcountantWindow)
 {
     ui->setupUi(this);
+    ui->addBook_btn->setIcon(QIcon("../pro1/pics/addBook.png"));
 }
 
 AcountantWindow::~AcountantWindow()
